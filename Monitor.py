caff099bf757306207e6609ce075100b6ffee3fb
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-20 19:47:39 +1000

    Getting stuff ready

--- Monitor.py
+++ Monitor.py
@@ -2,3 +2,27 @@ from bs4 import BeautifulSoup
 import requests
 import time
 import smtplib
+
+url = 'https://www.pccasegear.com/category/193_1901/graphics-cards/radeon-rx-vega-56'
+response = requests.get(url)
+
+soup = BeautifulSoup(response.content, 'html.parser')
+
+soup = soup.prettify()
+print(soup)
+
+price = soup.find_all('h3', class_ = 'product-price no-margin' )
+
+while True:
+    response = requests.get(url)
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+
+    soup = soup.prettify()
+    price = soup.find_all('h3', class_ = 'product-price no-margin' )
+    msg = 'Subject: Your Bot here. It looks like the price of the vega 56 on PCCG dropped below $620 aud'
+    fromaddr = ''
+    toaddrs = ''
+    for n in price:
+        if n < 620:
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Vega56priceMonitor
+You need to install Bs4 in order to use this. I may try to stand up a lxml version
