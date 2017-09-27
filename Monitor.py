7e26205fd5ede692ceb225a0e230570b84b87d37
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-27 10:51:48 +1000

    It's starting to work

--- Monitor.py
+++ Monitor.py
@@ -1,38 +1,34 @@
-from bs4 import BeautifulSoup
+from lxml import html
 import requests
 import time
 import smtplib
 
 url = 'https://www.pccasegear.com/category/193_1901/graphics-cards/radeon-rx-vega-56'
-response = requests.get(url)
 
-soup = BeautifulSoup(response.content, 'html.parser')
 
-soup = soup.prettify()
-print(soup)
 
-price = soup.find_all('h3', class_ = 'product-price no-margin' )
+
+
 
 while True:
     response = requests.get(url)
+    site = html.fromstring(response.content)
+    price = site.xpath('//h3[@class]/text()')
+    print(price)
 
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    soup = soup.prettify()
-    price = soup.find_all('h3', class_ = 'product-price no-margin' )
-    msg = 'Subject: Your Bot here. It looks like the price of the vega 56 on PCCG dropped below $620 aud'
-    fromaddr = ''
-    toaddrs = ''
-
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login("YOUR_EMAIL_ADDRESS", "YOUR_PASSWORD")
+    #msg = 'Subject: Your Bot here. It looks like the price of the vega 56 on PCCG dropped below $620 aud'
+    #fromaddr = ''
+    #toaddrs = ''
+    #Uncomment and replace things
+    #server = smtplib.SMTP('smtp.gmail.com', 587)
+    #server.starttls()
+    #server.login("YOUR_EMAIL_ADDRESS", "YOUR_PASSWORD")
     for n in price:
-        if n < 620:
+        if int(n) < 620:
             server.sendmail(fromaddr, toaddrs, msg)
             print("Price goal has been met")
             break
         else:
             print("Wel Nothing to report Yet")
-            time.sleep(60)
-            continue
+    time.sleep(60)
+    continue
