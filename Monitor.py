c5473986d865f014259463dcddd711412326d395
Cozza <32043188+darth22345@users.noreply.github.com>
2017-09-27 11:24:17 +1000

    I made stuff work
    It now checks the prices every minute and runs the provided check

--- Monitor.py
+++ Monitor.py
@@ -15,7 +15,7 @@ while True:
     site = html.fromstring(response.content)
     price = site.xpath('//h3[@class]/text()')
     print(price)
-
+    """UNCOMMENT AND INPUT EMAIL AND STUFF"""
     #msg = 'Subject: Your Bot here. It looks like the price of the vega 56 on PCCG dropped below $620 aud'
     #fromaddr = ''
     #toaddrs = ''
@@ -24,12 +24,12 @@ while True:
     #server.starttls()
     #server.login("YOUR_EMAIL_ADDRESS", "YOUR_PASSWORD")
     for n in price:
-        n = int(n)
+        n = int(n.replace("$", ""))
         if n < 620:
-            server.sendmail(fromaddr, toaddrs, msg)
+            #server.sendmail(fromaddr, toaddrs, msg)
             print("Price goal has been met")
             break
         else:
             print("Wel Nothing to report Yet")
-    time.sleep(60)
-    continue
+            time.sleep(60)
+            continue
